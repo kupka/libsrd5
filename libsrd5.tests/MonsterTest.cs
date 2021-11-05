59c07051eaf3f6ec7f18445075a01e802d0c3c89
Thomas Kupka <itchy.de@gmail.com>
2021-11-05 09:16:54 +0100

    Monster coverage

--- libsrd5.tests/MonsterTest.cs
+++ libsrd5.tests/MonsterTest.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using System;
 
 namespace srd5 {
     public class MonsterTest {
@@ -6,6 +7,17 @@ namespace srd5 {
         public void OgreTest() {
             Monster ogre = Monsters.Ogre;
             Assert.InRange(ogre.HitPoints, 28, 91);
+        }
+
+        [Fact]
+        public void EffectTest() {
+            Monster ogre = Monsters.Ogre;
+            Assert.Throws<NotImplementedException>(delegate {
+                ogre.AddEffect(Effect.PROTECTION);
+            });
+            Assert.Throws<NotImplementedException>(delegate {
+                ogre.RemoveEffect(Effect.PROTECTION);
+            });
         }
     }
 }

5ffc7e14924a394af74328b1940568dff5a2084c
Thomas Kupka <thomas@kupka.net>
2023-03-24 19:34:10 +0100

    Test coverage

---
+++ libsrd5.tests/AttackEffectTest.cs
@@ -0,0 +1,30 @@
+using Xunit;
+
+namespace srd5 {
+    public class AttackEffectTest {
+        private Monster uberMonster = new Monster(
+                    Monsters.Type.DRAGON, Monsters.ID.ORC, Alignment.LAWFUL_EVIL, 30, 30, 30, 30, 30, 30, 30, "1d6+10000", 40, 16,
+                    new Attack[] { }, new Attack[] { }, Size.HUGE
+                );
+
+        private Monster pansyMonster = new Monster(
+                    Monsters.Type.BEAST, Monsters.ID.GOAT, Alignment.LAWFUL_EVIL, 1, 1, 1, 1, 1, 1, 1, "1d6+10000", 40, 16,
+                    new Attack[] { }, new Attack[] { }, Size.HUGE
+                );
+
+
+        private void attackEffectTest(AttackEffect effect) {
+            for (int i = 0; i < 10; i++) {
+                effect.Invoke(Monsters.Aboleth, uberMonster);
+                effect.Invoke(Monsters.Aboleth, pansyMonster);
+            }
+        }
+
+        [Fact]
+        public void TestAttackEffects_A() {
+            attackEffectTest(Attacks.AbolethTentacleEffect);
+            attackEffectTest(Attacks.AssassinShortswordEffect);
+            attackEffectTest(Attacks.GiantScorpionStingEffect);
+        }
+    }
+}
